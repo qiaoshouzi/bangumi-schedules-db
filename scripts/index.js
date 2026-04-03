61223572e6641bf560bb2989c66fcaecd6df6d22
qiaoshouzi <qiaoshouzi@cfm.moe>
2026-04-03 22:49:09 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,7 @@ const season = 2
 const PUBLIC_HEADER = {
   'User-Agent': 'BangumiSchedules (+https://bgm.cfm.moe; https://github.com/qiaoshouzi/bangumi-schedules-db/blob/main/scripts/index.js)'
 }
-const PUBLIC_COOKIES = {
-  anime_schedule_wiki_lang: 'zh-CN',
-  anime_schedule_lang: 'zh-CN',
-}
-const langCode = 'zh-hans'
+const LANG_LIST = [['en', 'en'], ['zh-CN', 'zh-hans']]
 
 class API {
   token = undefined
@@ -31,12 +27,12 @@ class API {
     if (!resp.ok) throw new Error(`getToken: fetchError: ${resp.status}: ${resp.statusText}`)
     return this.getTokenFromHeaders(resp)
   }
-  async getData(url) {
+  async getData(url, langCookies = {}) {
     if (this.token === undefined) this.token = await this.getToken()
 
     const headers = new Headers()
     headers.set('x-public-api-token', this.token)
-    headers.set('Cookie', Object.entries(PUBLIC_COOKIES).map(([k, v]) => `${k}=${v}`).join('; '))
+    headers.set('Cookie', Object.entries(langCookies).map(([k, v]) => `${k}=${v}`).join('; '))
     for (const [k, v] of Object.entries(PUBLIC_HEADER)) headers.set(k, v)
     const resp = await fetch(url, { headers })
     if (!resp.ok) throw new Error(`getData: fetchError: ${resp.status}: ${resp.statusText}`)
@@ -45,21 +41,27 @@ class API {
   }
 }
 
-const main = async () => {
+const main = async (lang) => {
+  const langCookies = {
+    anime_schedule_wiki_lang: lang[0], // zh-CN en
+    anime_schedule_lang: lang[0],
+  }
+  const langCode = lang[1]
   const testFolderPath = path.join(import.meta.dirname, '../test/', `${year}-${season}`, langCode)
   fs.mkdirSync(testFolderPath, { recursive: true })
 
   const api = new API()
   const catalogDataFilePath = path.join(testFolderPath, 'data.json')
   let catalogData;
-  if (fs.existsSync(catalogDataFilePath)) {
+  const catalogDataIsFromLocal = fs.existsSync(catalogDataFilePath)
+  if (catalogDataIsFromLocal) {
     catalogData = JSON.parse(fs.readFileSync(catalogDataFilePath, 'utf-8'))
   } else {
-    catalogData = await api.getData(`https://bgm.wiki/api/season/${year}-${season}/catalog`)
+    catalogData = await api.getData(`https://bgm.wiki/api/season/${year}-${season}/catalog`, langCookies)
     fs.writeFileSync(catalogDataFilePath, JSON.stringify(catalogData), 'utf-8')
   }
   const itemsLength = catalogData.items.length
-  console.log(`Get Catalog data Done (${itemsLength})`)
+  console.log(`[${langCode}] Get Catalog data Done (${itemsLength}) (${catalogDataIsFromLocal ? 'LOCAL' : 'REMOTE'})`)
 
   const items = {}
   for (const index in catalogData.items) {
@@ -73,13 +75,14 @@ const main = async () => {
 
     const detailDataFilePath = path.join(testFolderPath, `${id}.json`)
     let detailData;
-    if (fs.existsSync(detailDataFilePath)) {
+    const detailDataIsFromLocal = fs.existsSync(detailDataFilePath)
+    if (detailDataIsFromLocal) {
       detailData = JSON.parse(fs.readFileSync(detailDataFilePath, 'utf-8'))
     } else {
-      detailData = await api.getData(`https://bgm.wiki/api/anime/${id}/detail`)
+      detailData = await api.getData(`https://bgm.wiki/api/anime/${id}/detail`, langCookies)
       fs.writeFileSync(detailDataFilePath, JSON.stringify(detailData), 'utf-8')
     }
-    console.log(`Get Detail ${id} Done (${Number(index) + 1}/${itemsLength})`)
+    console.log(`[${langCode}] Get Detail ${id} Done (${Number(index) + 1}/${itemsLength}) (${detailDataIsFromLocal ? 'LOCAL' : 'REMOTE'})`)
 
     const item = {
       isContinue,
@@ -102,4 +105,6 @@ const main = async () => {
   fs.mkdirSync(outputFolderPath, { recursive: true })
   fs.writeFileSync(path.join(outputFolderPath, `${year}-${season}.json`), JSON.stringify(items), 'utf-8')
 }
-main()
+(async () => {
+  for (const i of LANG_LIST) await main(i)
+})()
