0c8a8ef6485bf142df02aa6a0b0da0e97344a70a
qiaoshouzi <qiaoshouzi@cfm.moe>
2026-04-03 22:57:48 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -97,7 +97,7 @@ const main = async (lang) => {
       if (url && url !== '') platform.url = url
       const areas = ii.areasPrimary.map((v) => v.code)
       const time = ii.premiereTime
-      const licensors = ii.licensors.map((v) => v.displayName)
+      const licensors = ii.licensors?.map((v) => v.displayName) || []
       item.schedules.push({ platform, areas, time, licensors })
     }
     items[id] = item
